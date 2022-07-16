02db36a0186b76b16bb3f1ce7b907a2701fb9d0d
Your Name <you@example.com>
2022-07-16 16:34:30 -0700

    added more images to the the homepage

--- views/home.jsx
+++ views/home.jsx
@@ -5,16 +5,31 @@ function home () {
     return (
       <Def>
           <main>
-              <h1>HOME</h1>
+              <h1>REST-Rant</h1>
               <div>
                 <img src="/Images/Waffles Photo .jpg" alt= "Waffles Photo" />
                 <div>
                    Photo by <a href="AUTHOR_LINK">Taylor Kiser</a> on <a href="UNSPLASH_LINK">Unsplash</a>
                 </div>
               </div>
+              <div>
+                <img src="/Images/Pasta on Fork.jpg" alt= "Pasta wrapped into a upstanding fork Photo" />
+                <div>
+                   Photo by <a href="AUTHOR_LINK">Mae Mu</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+                </div>
+              </div>
+              <div>
+                <img src="/Images/dessert-cake.jpg" alt= "Chocolate and Almond Nute cake on a plate " />
+                <div>
+                   Photo by <a href="AUTHOR_LINK">Binyoussif</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+                </div>
+              </div>
+              
+
               <a href="/places">
-                <button className="btn-primary">Places Page</button>
+                <button className="btn-primary">Places </button>
               </a>
+              
         </main> 
       </Def>
     )
