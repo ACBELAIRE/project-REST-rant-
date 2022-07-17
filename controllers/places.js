ec2e9d14180e3d8faecf30fd298accaa22877e1b
Your Name <you@example.com>
2022-07-16 22:12:30 -0700

    adding and rendering a new view

--- controllers/places.js
+++ controllers/places.js
@@ -3,5 +3,9 @@ const router = require('express').Router()
 router.get('/', (req, res) => {
     res.render('places/index')
 })
+router.get('/new', (req, res) => {
+    res.render('places/new')
+  })
+  
 
 module.exports = router
