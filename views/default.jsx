39d3cb66c5470ca9d659d7823fd299e119f8dd4f
Your Name <you@example.com>
2022-10-12 21:17:38 -0700

    Navbar with style

--- views/default.jsx
+++ views/default.jsx
@@ -5,6 +5,19 @@ function Def (html) {
         <html>
             <head>
                 <title>Title</title>
+                <nav>
+                    <ul>
+                        <li>
+                            <a href="/">Home</a>
+                        </li>
+                        <li>
+                            <a href="/places"> Places</a>
+                        </li>
+                        <li>
+                            <a href="/places/new"> Add Place </a>
+                        </li>
+                    </ul>
+                </nav>
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" />
                 <link rel="stylesheet" href="/css/style.css"/>
             </head>
